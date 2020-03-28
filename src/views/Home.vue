68fdc35e7c928c631f7afe1b0ae410d07ee3e36e
Andreas Ambühl <36681000+AndiSwiss@users.noreply.github.com>
2020-03-28 16:50:37 +0100

    all elements are shown - but not nice layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,23 @@
     fluid
     class="home"
   >
-    content - tbd
+    <v-card
+      class="grey"
+    >
+      <v-row>
+        <img src="../assets/images/virus-outline-2.svg" alt="">
+        <v-col>
+          <h2>Quickly distribute 3D-Printed Masks to where they are needed!</h2>
+          <v-row
+            align="stretch"
+          >
+            <v-btn dark class="grey darken-2">I HAVE A 3D PRINTER</v-btn>
+            <v-btn dark class="grey darken-2">I NEED MASKS</v-btn>
+            <v-btn dark class="grey darken-2">I CAN DELIVER MASKS</v-btn>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-card>
   </v-container>
 </template>
 
