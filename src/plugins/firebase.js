32ca53662020664aecf43727fe292ec9f7619f83
Dario Breitenstein <online@imakethings.ch>
2020-03-28 17:12:48 +0100

    add development credentials

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -3,6 +3,13 @@ import 'firebase/auth';
 
 if (process.env.NODE_ENV === 'development') {
   const fbDevelopmentConfig = {
+    apiKey: 'AIzaSyASv70cPSrXiS796HT1_P96xOvaOwpeMzY',
+    authDomain: 'd-printing-dev.firebaseapp.com',
+    databaseURL: 'https://d-printing-dev.firebaseio.com',
+    projectId: 'd-printing-dev',
+    storageBucket: 'd-printing-dev.appspot.com',
+    messagingSenderId: '66781649147',
+    appId: '1:66781649147:web:eb17f370afda06fd8ab85e',
   };
 
   firebase.initializeApp(fbDevelopmentConfig);
