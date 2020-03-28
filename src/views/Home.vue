2bbdbc556acc54f39a94dca46f25069e1bfdeb35
Andreas Ambühl <36681000+AndiSwiss@users.noreply.github.com>
2020-03-28 20:00:04 +0100

    redefined layout of home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,30 +1,38 @@
 <template>
   <v-container
     fluid
-    class="home grey"
+    class="home grey pa-md-12"
   >
     <v-row>
       <img src="../assets/images/virus-outline-2.svg" alt="">
-      <v-col align-self="center">
-        <h2>Quickly distribute 3D-Printed Masks to where they are needed!</h2>
+      <v-col
+        align-self="center"
+        class="pa-8 ma-8"
+      >
+        <h2 class="headline pb-2">Quickly distribute 3D-Printed Masks to where they are needed!</h2>
         <v-row align="stretch">
           <v-col
             cols="12"
             md="auto"
           >
-            <v-btn dark class="grey darken-2">I HAVE A 3D PRINTER</v-btn>
+            <v-btn dark class="grey darken-2 text-uppercase">I have a 3d printer</v-btn>
           </v-col>
           <v-col
             cols="12"
             md="auto"
           >
-            <v-btn dark class="grey darken-2">I NEED MASKS</v-btn>
+            <v-btn dark class="grey darken-2 text-uppercase">I need masks</v-btn>
           </v-col>
           <v-col
             cols="12"
             md="auto"
           >
-            <v-btn dark class="grey darken-2">I CAN DELIVER MASKS</v-btn>
+            <v-btn dark class="grey darken-2 text-uppercase">I can deliver masks</v-btn>
+          </v-col>
+          <v-col
+            cols="12"
+            md="auto"
+          >
           </v-col>
         </v-row>
       </v-col>
@@ -37,8 +45,3 @@ export default {
   name: 'Home',
 };
 </script>
-
-
-<style scoped>
-
-</style>
