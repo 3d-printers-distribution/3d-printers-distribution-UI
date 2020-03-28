87a3ec043977bb6786c5e223c82ba1945dba9051
Andreas Ambühl <36681000+AndiSwiss@users.noreply.github.com>
2020-03-28 17:06:08 +0100

    layout enhanced a little bit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,34 @@
 <template>
   <v-container
     fluid
-    class="home"
+    class="home grey"
   >
-    <v-card
-      class="grey"
-    >
-      <v-row>
-        <img src="../assets/images/virus-outline-2.svg" alt="">
-        <v-col>
-          <h2>Quickly distribute 3D-Printed Masks to where they are needed!</h2>
-          <v-row
-            align="stretch"
+    <v-row>
+      <img src="../assets/images/virus-outline-2.svg" alt="">
+      <v-col align-self="center">
+        <h2>Quickly distribute 3D-Printed Masks to where they are needed!</h2>
+        <v-row align="stretch">
+          <v-col
+            cols="12"
+            md="auto"
           >
             <v-btn dark class="grey darken-2">I HAVE A 3D PRINTER</v-btn>
+          </v-col>
+          <v-col
+            cols="12"
+            md="auto"
+          >
             <v-btn dark class="grey darken-2">I NEED MASKS</v-btn>
+          </v-col>
+          <v-col
+            cols="12"
+            md="auto"
+          >
             <v-btn dark class="grey darken-2">I CAN DELIVER MASKS</v-btn>
-          </v-row>
-        </v-col>
-      </v-row>
-    </v-card>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
