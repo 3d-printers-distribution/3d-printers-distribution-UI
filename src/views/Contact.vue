7b0596e8c58fb3e1c76c1fa5ea93e7d94589a5c4
Andreas Ambühl <36681000+AndiSwiss@users.noreply.github.com>
2020-03-29 18:22:11 +0200

    implemented Contact.vue - profile pics still missing

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -11,7 +11,7 @@
       <v-flex xs12 sm6 md4 lg3 v-for="person in team" :key="person.name">
         <v-card class="text-center ma-3">
           <v-responsive class="pt-4">
-            <v-avatar size="200">
+            <v-avatar size="200" class="grey lighten-2">
               <v-icon size="200">mdi-account-circle</v-icon>
             </v-avatar>
           </v-responsive>
@@ -19,18 +19,47 @@
             <div class="title">{{ person.name }}</div>
             <div class="subtitle-1">{{ person.role }}</div>
             <div class="mt-4">
-              <v-tooltip bottom>
-                <template v-slot:activator="{ on }">
-                  <v-btn v-if="person.linkedin"
-                         large :href="person.linkedin"
-                         target="_blank"
-                         v-on="on"
-                  >
-                    <v-icon large>mdi-linkedin</v-icon>
-                  </v-btn>
-                </template>
-                <span>{{ person.linkedin }}</span>
-              </v-tooltip>
+              <v-row justify="center">
+                <v-tooltip bottom>
+                  <template v-slot:activator="{ on }">
+                    <v-btn v-if="person.linkedin"
+                           large :href="person.linkedin"
+                           target="_blank"
+                           v-on="on"
+                           class="mx-2"
+                    >
+                      <v-icon large>mdi-linkedin</v-icon>
+                    </v-btn>
+                  </template>
+                  <span>{{ person.linkedin }}</span>
+                </v-tooltip>
+                <v-tooltip bottom>
+                  <template v-slot:activator="{ on }">
+                    <v-btn v-if="person.github"
+                           large :href="person.github"
+                           target="_blank"
+                           v-on="on"
+                           class="mx-2"
+                    >
+                      <v-icon large>mdi-github</v-icon>
+                    </v-btn>
+                  </template>
+                  <span>{{ person.github }}</span>
+                </v-tooltip>
+                <v-tooltip bottom>
+                  <template v-slot:activator="{ on }">
+                    <v-btn v-if="person.website"
+                           large :href="person.website"
+                           target="_blank"
+                           v-on="on"
+                           class="mx-2"
+                    >
+                      <v-icon large>mdi-link-box-variant</v-icon>
+                    </v-btn>
+                  </template>
+                  <span>{{ person.website }}</span>
+                </v-tooltip>
+              </v-row>
             </div>
           </v-card-text>
         </v-card>
@@ -45,15 +74,76 @@ export default {
   data() {
     return {
       team: [
+        // // Empty template:
+        // {
+        //   name: '',
+        //   role: '',
+        //   linkedin: '',
+        //   github: '',
+        // },
+        {
+          name: 'Adrian Lehmann',
+          role: 'Backend Developer',
+          linkedin: 'https://www.linkedin.com/in/adrian-lehmann',
+          github: 'https://github.com/adrianleh',
+        },
         {
           name: 'Andreas Ambühl',
-          role: 'Front-end developer',
-          link: 'https://github.com/AndiSwiss',
-          linkedin: 'https://www.linkedin.com/in/andreas-ambuehl/',
+          role: 'Front End Developer',
+          linkedin: 'https://www.linkedin.com/in/andreas-ambuehl',
+          github: 'https://github.com/AndiSwiss',
         },
-        { name: 'Dario Breitenstein', role: 'Front-end developer' },
-        { name: 'Hanna Lisa Franz', role: 'Front-end developer' },
-        { name: 'Luca Fluri', role: 'Front-end developer' },
+        {
+          name: 'Apostolos Staridis',
+          role: 'QA & Marketing',
+          linkedin: 'https://www.linkedin.com/in/aposstar1994',
+        },
+        {
+          name: 'Dario Breitenstein',
+          role: 'Front End Developer',
+          linkedin: 'https://www.linkedin.com/in/dario-breitenstein-397873a6',
+          github: 'https://github.com/chdabre',
+        },
+        {
+          name: 'Florent Bardy',
+          role: 'Product Design & Marketing',
+          linkedin: 'https://www.linkedin.com/in/florent-bardy-345263a',
+          github: 'https://github.com/Flobar42',
+        },
+        {
+          name: 'Hanna Lisa Franz',
+          role: 'Front End Developer',
+          github: 'https://github.com/nightbehaviour',
+        },
+        {
+          name: 'Hussein Duvigneau',
+          role: 'Front End Developer',
+          linkedin: 'https://www.linkedin.com/in/husseinduvigneau',
+          website: 'https://notmybase.com',
+        },
+        {
+          name: 'Luca Fluri',
+          role: 'Front End Developer',
+          linkedin: 'https://www.linkedin.com/in/luca-fluri-9a7286172',
+          github: 'https://github.com/lucafluri',
+        },
+        {
+          name: 'Manuel Ramirez Lopez',
+          role: 'DevOps',
+          linkedin: 'https://www.linkedin.com/in/manuel-ramirez-lopez',
+          github: 'https://github.com/chumbo4',
+        },
+        {
+          name: 'Paschalis Dimitriou',
+          role: 'Project Leader',
+          linkedin: 'https://www.linkedin.com/in/paschalis-dimitriou-57786a61',
+        },
+        {
+          name: 'Sebastian Markgraf',
+          role: 'Backend Developer',
+          linkedin: 'https://www.linkedin.com/in/sebastian-markgraf-85726a182/',
+          github: 'https://github.com/sebimarkgraf',
+        },
       ],
     };
   },
