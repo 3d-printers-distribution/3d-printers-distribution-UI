4b0d8d8a987c2e295763b08949b909bdbffc1388
Dario Breitenstein <online@imakethings.ch>
2020-03-29 16:08:46 +0200

    wip

--- src/views/Onboarding.vue
+++ src/views/Onboarding.vue
@@ -5,28 +5,45 @@
         <v-card>
           <v-card-title>Get Started</v-card-title>
           <v-card-text>
-              <p class="body-1">Before you can begin, we need to know some details:</p>
+              <p class="body-2">Before you can begin, we need to know some details:</p>
               <v-form ref="onboarding-form">
-                <v-text-field
-                  filled
-                  label="Full name"
-                  required
-                ></v-text-field>
-                <v-text-field
-                  filled
-                  label="Phone Number"
-                  required
-                ></v-text-field>
-                <v-text-field
-                  filled
-                  label="Address"
-                  required
-                ></v-text-field>
-                <v-text-field
-                  filled
-                  label="City"
-                  required
-                ></v-text-field>
+                <section>
+                  <v-text-field
+                    filled
+                    label="Full name*"
+                    required
+                  ></v-text-field>
+                  <v-text-field
+                    filled
+                    label="Phone Number*"
+                    required
+                  ></v-text-field>
+
+                  <span class="caption">* Required</span>
+                </section>
+
+                <section class="mt-4">
+                  <h3 class="title">Address</h3>
+                  <p class="body-2">
+                    We share your address with distributors
+                    to help them pick up or drop off materials.
+                  </p>
+                  <v-text-field
+                    filled
+                    label="Address"
+                    required
+                  ></v-text-field>
+                  <v-text-field
+                    filled
+                    label="City"
+                    required
+                  ></v-text-field>
+                </section>
+                <v-btn
+                  large block
+                  color="primary"
+                  type="submit"
+                >Submit</v-btn>
               </v-form>
           </v-card-text>
         </v-card>
@@ -38,6 +55,36 @@
 <script>
 export default {
   name: 'Onboarding',
+  data() {
+    return {
+      onboardingForm: {
+        name: {
+          field: '',
+          rules: [
+            (v) => !!v || this.$t('forms.validation.requiredField'), // Check if field is empty
+          ],
+        },
+        phone: {
+          field: '',
+          rules: [
+            (v) => !!v || this.$t('forms.validation.requiredField'), // Check if field is empty
+          ],
+        },
+        address: {
+          field: '',
+          rules: [
+            (v) => !!v || this.$t('forms.validation.requiredField'), // Check if field is empty
+          ],
+        },
+        city: {
+          field: '',
+          rules: [
+            (v) => !!v || this.$t('forms.validation.requiredField'), // Check if field is empty
+          ],
+        },
+      },
+    };
+  },
 };
 </script>
 
