9794fcde43ad3ae9c631678b75f886a9917a4f48
Hussein duvigneau <hussein@notmybase.com>
2020-03-29 11:43:50 +0200

    sample data

---
+++ mocks/raw/supply.js
@@ -0,0 +1,18 @@
+/* eslint-disable object-curly-newline */
+export default [
+  { id: 1, location: 'Oerlikon', distanceKm: 14.5, quantity: 100 },
+  { id: 2, location: 'Oerlikon', distanceKm: 14.5, quantity: 23 },
+  { id: 3, location: 'Langstrasse', distanceKm: 3.2, quantity: 10 },
+  { id: 4, location: 'Rehalp', distanceKm: 1.2, quantity: 10 },
+  { id: 5, location: 'Opernhaus', distanceKm: 0.2, quantity: 100 },
+  { id: 6, location: 'Mike\'s house', distanceKm: 1.5, quantity: 4 },
+  { id: 7, location: 'Stadelhofen', distanceKm: 0.4, quantity: 14 },
+  { id: 8, location: 'Oerlikon', distanceKm: 14.2, quantity: 31 },
+  { id: 9, location: 'Town Hall', distanceKm: 1.0, quantity: 17 },
+  { id: 10, location: 'Langstrasse', distanceKm: 2.9, quantity: 52 },
+  { id: 11, location: 'Wiedikon', distanceKm: 2.6, quantity: 6 },
+  { id: 12, location: 'Google HQ', distanceKm: 15.1, quantity: 12 },
+  { id: 13, location: 'Adrian\'s House', distanceKm: 5.2, quantity: 3 },
+  { id: 14, location: 'HB Locker-room', distanceKm: 4.5, quantity: 13 },
+  { id: 15, location: 'Hotel Mercure', distanceKm: 8.5, quantity: 12 },
+];
