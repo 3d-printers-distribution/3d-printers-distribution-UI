0dfbb6698233b9336abcc3b7d81f7eed5067f791
Hussein duvigneau <hussein@notmybase.com>
2020-03-29 12:29:47 +0200

    mock data
    forgot to add these files previously

---
+++ mocks/raw/demand.js
@@ -0,0 +1,7 @@
+/* eslint-disable object-curly-newline */
+export default [
+  { id: 1, location: 'University Hospital, Zh', distanceKm: 6.4, quantity: 220 },
+  { id: 2, location: 'VBZ Depot', distanceKm: 12.1, quantity: 58 },
+  { id: 3, location: 'Kinderspital Zh', distanceKm: 3.2, quantity: 123 },
+  { id: 4, location: 'Airport', distanceKm: 31.0, quantity: 200 },
+];
