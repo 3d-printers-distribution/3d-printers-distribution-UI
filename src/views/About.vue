abfe346bee10c66bf19fef4d82e3b5da0c54b148
Luca Fluri <luca.fluri4@gmail.com>
2020-03-29 16:45:59 +0200

    fix bad html

--- src/views/About.vue
+++ src/views/About.vue
@@ -8,23 +8,23 @@
           3DtoMeds was born out of the realisation that medical centers throughout Europe
           are facing a very severe scarcity of personal protective equipment to fight the coronavirus crisis.
         </span>
-        <br>
-        Our team is united by a common vision: to design an efficient platform to urgently connect the 3D
-        printing community to the closest medical centers in need and to ensure fast and reliable delivery.
-
-        <p> <span class="font-italic">“Our first priority right now needs to be maximising the availability of personal protective
-        equipment in Europe, especially masks and medical devices such as ventilators,” </span> 
-        <br>-- Thierry Breton, European Commissioner for Internal Market and Services - The Financial Times, March 20th 2020 </p> 
-
-        We are convinced that unlocking the production power of the 3D printing community will make a difference in the COVID19 crisis but ultimately with most of the european population in confinement today, delivery is critical.
-        <br>
-        Our platform connects pharmacies and medical centers that are in need of personal protective equipment and medical devices with 3D printers and delivery men in their neighbourhood.
+          <br>
+          Our team is united by a common vision: to design an efficient platform to urgently connect the 3D
+          printing community to the closest medical centers in need and to ensure fast and reliable delivery.
+          <br><br>
+          <span class="font-italic">“Our first priority right now needs to be maximising the availability of personal protective
+          equipment in Europe, especially masks and medical devices such as ventilators,” </span> 
+          <br>-- Thierry Breton, European Commissioner for Internal Market and Services - The Financial Times, March 20th 2020
+          <br><br>
+          We are convinced that unlocking the production power of the 3D printing community will make a difference in the COVID19 crisis but ultimately with most of the european population in confinement today, delivery is critical.
+          <br>
+          Our platform connects pharmacies and medical centers that are in need of personal protective equipment and medical devices with 3D printers and delivery men in their neighbourhood.
+        </p>
         <ul>
           <li>Medical centers and pharmacies will have a clear view on the available stocks of personal protective equipment around them</li>
           <li>All available 3D printers equipped will be able to make themselves visible to accelerate the production of qualified 3D printed parts, where they’re needed, when they’re needed.</li>
           <li>Supply Chain will be shortened and delivery made faster. Delivery men, professional, volunteers and civil servant will be recruited to join the movement</li>
         </ul>
-      </p>
     </div>
     <!-- eslint-enable -->
   </v-container>
