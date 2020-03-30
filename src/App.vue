89f0ddffe7eee299e50ca47641c146dc021ecad4
Andreas Ambühl <36681000+AndiSwiss@users.noreply.github.com>
2020-03-30 14:41:08 +0200

    exported NavBar to separate component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      flat
-    >
-      <v-toolbar-title><router-link style="text-decoration:none;color:black;"
-      :to="{ name: 'home' }"> 3DtoMeds</router-link></v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn v-if="userIsLoggedIn" text :to="{ name: 'dashboard' }">Dashboard</v-btn>
-      <v-btn text :to="{ name: 'team' }">Team</v-btn>
-      <v-btn text :to="{ name: 'about' }">About</v-btn>
-      <v-btn text v-if="userIsLoggedIn" @click="logoutPlease">Logout</v-btn>
-      <v-btn text v-else :to="{name: 'login'}">Login</v-btn>
-    </v-app-bar>
+    <NavBar />
 
     <v-content>
       <v-container fluid v-if="userToken">
@@ -63,8 +51,10 @@
 
 <script>
 import { mapState } from 'vuex';
+import NavBar from './components/NavBar.vue';
 
 export default {
+  components: { NavBar },
   name: 'App',
   computed: {
     ...mapState(['userToken']),
---
+++ src/components/NavBar.vue
@@ -0,0 +1,29 @@
+<template>
+      <v-app-bar
+        app
+        flat
+      >
+        <v-toolbar-title><router-link style="text-decoration:none;color:black;"
+        :to="{ name: 'home' }"> 3DtoMeds</router-link></v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn v-if="userIsLoggedIn" text :to="{ name: 'dashboard' }">Dashboard</v-btn>
+        <v-btn text :to="{ name: 'team' }">Team</v-btn>
+        <v-btn text :to="{ name: 'about' }">About</v-btn>
+        <v-btn text v-if="userIsLoggedIn" @click="logoutPlease">Logout</v-btn>
+        <v-btn text v-else :to="{name: 'login'}">Login</v-btn>
+      </v-app-bar>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
