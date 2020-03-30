260b12e3b5350235bb9b3e04df404b67399bfa48
Dario Breitenstein <online@imakethings.ch>
2020-03-30 15:56:15 +0200

    wip

--- src/views/Onboarding.vue
+++ src/views/Onboarding.vue
@@ -3,7 +3,7 @@
     <v-row class="justify-center">
       <v-col md="10" lg="8">
         <v-card>
-          <v-card-title>Get Started as a {{ userRole }}</v-card-title>
+          <v-card-title>Get Started</v-card-title>
           <v-card-text>
               <p class="body-2">Before you can begin, we need to know some details:</p>
               <v-form ref="onboarding-form">
@@ -31,9 +31,36 @@
                     v-model="basicForm.email.field"
                     :rules="basicForm.email.rules"
                   ></v-text-field>
-
                   <span class="caption">* Required</span>
                 </section>
+
+                <section class="mt-4">
+                  <span class="title">How would you like to contribute?</span><br/>
+                  <span class="caption">You will be able to change this later.</span>
+                  <v-chip-group v-model="roleSelection">
+                    <v-chip
+                      v-for="role in userRoles"
+                      :key="role.value"
+                      v-text="role.text"
+                      class="font-weight-medium"
+                      active-class="primary--text"
+                    ></v-chip>
+                  </v-chip-group>
+                </section>
+
+                <section
+                  class="consumer-form"
+                  v-if="roleFromSelection === 'consumer'"
+                >
+                  <GeolocationInput @input="setGeolocation"/>
+                </section>
+                <section
+                  class="supplier-form"
+                  v-if="roleFromSelection === 'supplier'"
+                >
+
+                </section>
+
                 <v-btn
                   large block
                   color="primary"
@@ -48,10 +75,30 @@
 </template>
 
 <script>
+import GeolocationInput from './GeolocationInput.vue';
+
+const userRoles = [
+  {
+    value: 'consumer',
+    text: 'I need face shields',
+  },
+  {
+    value: 'producer',
+    text: 'I can supply face shields (by 3D Printing)',
+  },
+  {
+    value: 'distributor',
+    text: 'I can distribute face shields',
+  },
+];
+
 export default {
   name: 'Onboarding',
+  components: { GeolocationInput },
   data() {
     return {
+      userRoles,
+      roleSelection: 0,
       basicForm: {
         name: {
           field: '',
@@ -73,15 +120,30 @@ export default {
           ],
         },
       },
+      consumerForm: {
+        location: { lat: 0, lng: 0 },
+        locationType: {
+          value: '',
+          choices: [
+            { text: 'Hospital', value: 'HOSPITAL' },
+            { text: 'Pharmacy', value: 'PHARMACY' },
+          ],
+        },
+      },
+      producerForm: {
+        location: { lat: 0, lng: 0 },
+      },
     };
   },
   computed: {
-    userRole() {
-      return this.$route.query.type || 'consumer';
+    roleFromSelection() {
+      return userRoles[this.roleSelection].value;
+    },
+  },
+  methods: {
+    setGeolocation(location) {
+      console.log(location);
     },
   },
 };
 </script>
-
-<style scoped>
-</style>
