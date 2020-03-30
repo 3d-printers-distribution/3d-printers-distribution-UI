65c81cfc240c7121bed95faa0b1a9773c5b62239
Andreas Ambühl <36681000+AndiSwiss@users.noreply.github.com>
2020-03-30 16:11:47 +0200

    user drawer working - but not I don't get it more functional yet

--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -1,7 +1,18 @@
 <template>
   <v-navigation-drawer v-if="userIsLoggedIn" app v-model="drawer" class="primary lighten-3">
-    <p> user: {{user}}
-    </p>
+    <v-layout column align-center>
+      <v-flex class="mt-5">
+        <v-avatar size="100" class="grey lighten-2">
+          <v-icon size="100">mdi-account-circle</v-icon>
+        </v-avatar>
+      </v-flex>
+      <v-flex class="mt-5">
+        <div class="subtitle-1">{{user.email}}</div>
+      </v-flex>
+    </v-layout>
+
+<!--    For printing out the whole user-object from the state:-->
+<!--    <p class="mt-4 ml-1 red&#45;&#45;text text&#45;&#45;darken-4"> user: {{user}}</p>-->
   </v-navigation-drawer>
 </template>
 
