fce17cbdcd29bb41dee5b6982de74cb79bb97d70
Andreas Ambühl <36681000+AndiSwiss@users.noreply.github.com>
2020-03-30 10:09:26 +0200

    added message that site is still work in progress

--- src/App.vue
+++ src/App.vue
@@ -23,6 +23,40 @@
       </v-container>
       <router-view></router-view>
     </v-content>
+    <v-footer
+      dark
+      padless
+    >
+      <v-container
+        fluid
+        class="red lighten-2 pa-md-12 my-4 text-center"
+      >
+        <div class="headline">
+          30.3.2020: This site is not productive yet. We are still building the front- and back-end.
+          Feel free to explore the site to test it's already existing functionality.
+        </div>
+      </v-container>
+      <v-card
+        flat
+        tile
+        class="indigo lighten-1 white--text text-center v-picker--full-width"
+      >
+        <v-card-text>
+          <v-btn
+            v-for="icon in icons"
+            :key="icon"
+            class="mx-4 white--text"
+            icon
+          >
+            <v-icon size="24px">{{ icon }}</v-icon>
+          </v-btn>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-text class="white--text">
+          {{ new Date().getFullYear() }} — <strong>3DtoMeds</strong>
+        </v-card-text>
+      </v-card>
+    </v-footer>
   </v-app>
 </template>
 
