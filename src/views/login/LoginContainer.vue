8335d5dcd8931947409c7fd14cf3b1b94d3d4778
nightbehaviour <hannalisa.franz@students.fhnw.ch>
2020-03-30 12:43:02 +0200

    favicon, name, styling, logo

--- src/views/login/LoginContainer.vue
+++ src/views/login/LoginContainer.vue
@@ -1,12 +1,13 @@
 <template>
   <v-container class="login-container white d-flex flex-column">
     <!-- LOGO -->
-    <v-row class="logo-container">
-      <v-col class="d-flex justify-center flex-column">
-        <img alt="Virus" src="../../assets/virus.svg" />
+    <v-row class="logo-container justify-center">
+      <v-col class="d-flex flex-column" sm="8" lg="5">
+        <div>
+          <img class="logo" alt="3DtoMeds" src="../../assets/logo.jpg" />
+        </div>
       </v-col>
     </v-row>
-
     <!-- FORM CONTAINER adheres to bottom -->
     <v-row justify="center" class="form-container flex-grow-0 text-center">
       <v-col align-self="end" lg="4" md="5" sm="8">
@@ -27,5 +28,8 @@ export default {
   .login-container {
     height: 100%;
     max-width: unset !important;
+  }
+  .logo{
+    width: 100%;
   }
 </style>
