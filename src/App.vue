960c178fd67a3867ee933bfffe89e6cb01b229e4
Andreas Ambühl <36681000+AndiSwiss@users.noreply.github.com>
2020-03-30 10:12:59 +0200

    added 'dashboard'-button and converted title to be clickable to return to the home-screen

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,11 @@
       app
       flat
     >
-      <v-toolbar-title>3DtoMeds</v-toolbar-title>
+      <v-toolbar-title><v-btn :to="{ name: 'home' }"> 3DtoMeds</v-btn></v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn text :to="{ name: 'home' }">Home</v-btn>
       <v-btn text :to="{ name: 'team' }">Team</v-btn>
       <v-btn text :to="{ name: 'about' }">About</v-btn>
+      <v-btn text :to="{ name: 'dashboard' }">Dashboard</v-btn>
       <v-btn text v-if="userIsLoggedIn" @click="logoutPlease">Logout</v-btn>
       <v-btn text v-else :to="{name: 'login'}">Login</v-btn>
     </v-app-bar>
