2523ef532faef115c6c9377b0f2b149719011bf0
nightbehaviour <hannalisa.franz@students.fhnw.ch>
2020-03-30 16:48:40 +0200

    just cosmetics

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container fluid class="home-container white d-flex flex-column">
+  <v-container fluid class="white d-flex flex-column">
     <!-- LOGO -->
     <v-row class="justify-center">
       <v-col sm="8" lg="5">
@@ -9,10 +9,15 @@
       </v-col>
     </v-row>
     <v-row class="justify-center">
-      <v-col class="d-flex flex-column" sm="8">
+      <v-col class="d-flex flex-column align-center" sm="8">
         <h2 class="headline pb-2 text-center">
           Empowering the 3D printing community to fight COVID-19!</h2>
-        <div class="d-flex flex-row flex-wrap justify-center">
+          <div class="subtitle-1 text-center my-4">
+            Join us, if you can help as a supplier (3d-printer-owner), as a
+            demanding facility (hospital, dental clinic, pharmacy,...) or as a distributor
+            (volunteer, civil servant).
+          </div>
+        <div>
           <v-btn
             dark color="primary"
             class="mr-4 mt-4"
@@ -20,15 +25,10 @@
           >
             Signup to contribute
           </v-btn>
-          <div class="subtitle-1 text-center my-4">
-            Join us, if you can help as a supplier (3d-printer-owner), as a
-            demanding facility (hospital, dental clinic, pharmacy,...) or as a distributor
-            (volunteer, civil servant, ...).
-          </div>
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/xkrW_oU3lxA"
-                  frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope;
-                  picture-in-picture" allowfullscreen></iframe>
         </div>
+          <iframe class="iFrame" width="560" height="315" src="https://www.youtube.com/embed/xkrW_oU3lxA"
+                  allow="accelerometer; autoplay; encrypted-media; gyroscope;
+                  picture-in-picture" allowfullscreen></iframe>
       </v-col>
     </v-row>
   </v-container>
@@ -42,5 +42,10 @@ export default {
 <style lang="scss">
   .logo{
     width: 100%;
+  }
+  .iFrame{
+    margin-bottom: 4rem;
+    margin-top: 4rem;
+    border: none;
   }
 </style>
